7a19e12214a33bd9b218a84061169989ceaebc1a
Vexy <90870585+vexypt@users.noreply.github.com>
2025-09-11 23:30:25 +0100

    Initial Commit

---
+++ ClickCount/ClickCountApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickCountApp.swift
+//  ClickCount
+//
+//  Created by Vexy on 11/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClickCountApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
