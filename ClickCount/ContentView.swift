99088b9cba00ceb0b855aa3c96f8eba5342b2daf
Vexy <90870585+vexypt@users.noreply.github.com>
2025-09-11 23:44:07 +0100

    Finish

--- ClickCount/ContentView.swift
+++ ClickCount/ContentView.swift
@@ -8,12 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var counter = 0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 30) {
+            
+            Text("Clicks: \(counter)")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+            
+            Button(action: {
+                counter += 1
+            }) {
+                Text("Tap me!")
+                    .font(.title)
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 5)
+            }
+            
         }
         .padding()
     }
